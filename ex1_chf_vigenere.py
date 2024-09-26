c559a7b93c6702466a2acef877e5fc5a5d37a0de
XterS <158148388+LeoGaborit@users.noreply.github.com>
2024-09-26 21:13:47 +0200

    coding : mid commit

--- ex1_chf_vigenere.py
+++ ex1_chf_vigenere.py
@@ -4,24 +4,36 @@ def chiffrementVigenere(motAchiffrer : str, clef : str) -> str:
     Postconditions: retourne le mot chiffré"""
 
     for car in range(len(motAchiffrer)):
-        carAlphab : str
+
+        # Variables
+        codeAlphabet : int
         carASCII : int
+        carCle : int
+        i : int
+        code : int
 
         motChiffre : [int]
         cleChiffre : [int]
-        lstResultat : [str]
+        lstInt : [str]
+        lstStr : [str]
         strResultat : str
 
-        carAlphabet = ""
+        # Initialisation
+        codeAlphabet = 0
         carASCII = 0
+        carCle = 0
+        i = 0
+        code = 0
+
         motChiffre = []
         cleChiffre = []
-        lstResultat = []
+        lstInt = []
+        lstStr = []
         strResultat = ""
 
         # Parcours du mot à chiffrer
-        for car in range(len(motAchiffrer)):
-            carASCII = ord(motAchiffrer[car])
+        for car in motAchiffrer:
+            carASCII = ord(car)
             if not ((65 <= carASCII <= 90) or (97 <= carASCII <= 122)): # Test : Si caractère n'est pas entre a-z ou A-Z
 
                 #  On vérifie si car spécial
@@ -54,19 +66,40 @@ def chiffrementVigenere(motAchiffrer : str, clef : str) -> str:
                 else : # Caractère spécial intraitable
                     return 'Caractère spécial intraitable'
 
-            else:
                 motChiffre.append(carASCII)
 
-            for carASCII in range(len(clef)):
-                cleChiffre.append(ord(clef[carASCII]))
+            if carASCII % 2 != 0: # Si impair
+                motChiffre.append(carASCII-1)
+            else : # Si pair
+                motChiffre.append(carASCII)
 
-            # Chiffrement
-            for car in range(len(motChiffre)):
-                lstResultat.append(chr(motChiffre[car] + cleChiffre[car % len(cleChiffre)]))
-    print(lstResultat)
+        for car in clef:
+            if ord(car) % 2 != 0: # Si impair
+                cleChiffre.append(ord(car)-1)
+            else : # Si pair
+                cleChiffre.append(ord(car))
 
-    return motChiffre
 
-chiffrer = "Bonjour"
+        # Chiffrement
+        for i in range(len(motChiffre)):  # On chiffre le mot
+            carCle = cleChiffre[i % len(cleChiffre)]
+
+            if 65 <= motChiffre[i] <= 90:  # Majuscule
+                codeAlphabet = (motChiffre[i] - 64 + carCle - 64) % 26 + 64 # +64 pour revenir à la table ASCII (majuscule)
+
+            elif 97 <= motChiffre[i] <= 122:  # Minuscule
+                codeAlphabet = (motChiffre[i] - 96 + carCle - 96) % 26 + 96 # +96 pour revenir à la table ASCII (minuscule)
+
+            lstInt.append(codeAlphabet)
+
+        for code in lstInt:  # On convertit les chiffres en lettres
+            lstStr.append(chr(code))
+
+        strResultat = ''.join(lstStr)  # On concatène les lettres
+
+        return motAchiffrer + " chiffré à l'aide de la clé " + clef + " donne : " + strResultat
+
+chiffrer = "Texteclair"
 clef = "Clef"
-chiffrementVigenere(chiffrer, clef)
+print(chiffrementVigenere(chiffrer, clef))
+
